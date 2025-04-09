df3ba60eb7f3d3cdab9897f62e13b467d8747b33
Mikubu39 <nicek92408@gmail.com>
2025-04-09 15:56:58 +0700

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'secretive-treasure-scooter.glitch.me/api';
 
 export const login = (username, password) =>
   axios.post(`${API_BASE_URL}/login`, { username, password });
